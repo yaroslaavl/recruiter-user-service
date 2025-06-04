a6b7bd0f3d144c4ccc1693721b1c58854833c08f
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-06-04 23:54:11 +0200

    04.06.2025 - added @JsonProperty

--- src/main/java/org/yaroslaavl/userservice/dto/AuthTokenDto.java
+++ src/main/java/org/yaroslaavl/userservice/dto/AuthTokenDto.java
@@ -1,12 +1,13 @@
 package org.yaroslaavl.userservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record AuthTokenDto(String accessToken,
-                           Long expiresIn,
-                           String refreshToken,
-                           Long refreshExpiresIn) implements Serializable {
+public record AuthTokenDto(@JsonProperty("access_token") String accessToken,
+                           @JsonProperty("expires_in") Long expiresIn,
+                           @JsonProperty("refresh_token") String refreshToken,
+                           @JsonProperty("refresh_expires_in") Long refreshExpiresIn) implements Serializable {
 }

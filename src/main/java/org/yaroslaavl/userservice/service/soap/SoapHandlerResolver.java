96b4c6613e3e84bcdc6e44f24d8109ca52118f6b
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-06-11 20:27:21 +0200

    11.06.2025 - added soap integration to GUS API

---
+++ src/main/java/org/yaroslaavl/userservice/service/soap/SoapHandlerResolver.java
@@ -0,0 +1,18 @@
+package org.yaroslaavl.userservice.service.soap;
+
+import jakarta.xml.ws.handler.Handler;
+import jakarta.xml.ws.handler.HandlerResolver;
+import jakarta.xml.ws.handler.PortInfo;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class SoapHandlerResolver implements HandlerResolver {
+
+    @Override
+    public List<Handler> getHandlerChain(PortInfo portInfo) {
+        return Collections.singletonList(new SoapMessageHandler());
+    }
+}

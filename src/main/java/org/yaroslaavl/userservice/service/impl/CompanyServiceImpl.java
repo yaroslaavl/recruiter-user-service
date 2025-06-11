ab95908f2b809760cfbf1fd757764e694ce02362
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-06-11 20:28:51 +0200

    11.06.2025 - added nip verification soap request and other functionalities

---
+++ src/main/java/org/yaroslaavl/userservice/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,49 @@
+package org.yaroslaavl.userservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yaroslaavl.userservice.database.entity.Company;
+import org.yaroslaavl.userservice.database.entity.enums.CompanyStatus;
+import org.yaroslaavl.userservice.database.repository.CompanyRepository;
+import org.yaroslaavl.userservice.dto.integrations.CompanyExecutedDto;
+import org.yaroslaavl.userservice.service.CompanyService;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    @Override
+    @Transactional
+    public Company createOrGet(CompanyExecutedDto companyExecutedDto) {
+        String nip = companyExecutedDto.getNip();
+
+        Optional<Company> companyByNip =
+                companyRepository.findCompanyByNip(nip);
+
+        if (companyByNip.isEmpty()) {
+            Company company = Company.builder()
+                    .name(companyExecutedDto.getName())
+                    .nip(nip)
+                    .city(companyExecutedDto.getCity())
+                    .postCode(companyExecutedDto.getPostCode())
+                    .street(companyExecutedDto.getStreet())
+                    .voivodeship(companyExecutedDto.getVoivodeship())
+                    .companyStatus(CompanyStatus.PENDING)
+                    .build();
+
+            companyRepository.saveAndFlush(company);
+
+            return company;
+        }
+
+        return companyByNip.get();
+    }
+}
---
+++ src/main/java/org/yaroslaavl/userservice/service/impl/RecruiterRegistrationRequestServiceImpl.java
@@ -0,0 +1,39 @@
+package org.yaroslaavl.userservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yaroslaavl.userservice.database.entity.Company;
+import org.yaroslaavl.userservice.database.entity.Recruiter;
+import org.yaroslaavl.userservice.database.entity.RecruiterRegistrationRequest;
+import org.yaroslaavl.userservice.database.entity.enums.RequestStatus;
+import org.yaroslaavl.userservice.database.repository.RecruiterRegistrationRequestRepository;
+import org.yaroslaavl.userservice.exception.RecruiterRequestCreatedException;
+import org.yaroslaavl.userservice.service.RecruiterRegistrationRequestService;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RecruiterRegistrationRequestServiceImpl implements RecruiterRegistrationRequestService {
+
+    private final RecruiterRegistrationRequestRepository recruiterRegistrationRequestRepository;
+
+    @Override
+    @Transactional
+    public Boolean create(Company company, Recruiter recruiter) {
+        if (recruiterRegistrationRequestRepository.existsByRecruiterId(recruiter.getId())) {
+            throw new RecruiterRequestCreatedException("Recruiter has already sent a request for an account");
+        }
+
+        RecruiterRegistrationRequest recruiterRegistrationRequest = RecruiterRegistrationRequest.builder()
+                .recruiter(recruiter)
+                .company(company)
+                .requestStatus(RequestStatus.PENDING)
+                .build();
+
+        recruiterRegistrationRequestRepository.save(recruiterRegistrationRequest);
+        return true;
+    }
+
+}

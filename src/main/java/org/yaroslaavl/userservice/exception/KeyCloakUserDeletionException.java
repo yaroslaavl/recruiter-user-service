410f210086e1d0fc295a03f39f27a53f3b7bee27
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-06-16 21:14:31 +0200

    16.06.2025 - created custom exception

---
+++ src/main/java/org/yaroslaavl/userservice/exception/KeyCloakUserDeletionException.java
@@ -0,0 +1,7 @@
+package org.yaroslaavl.userservice.exception;
+
+public class KeyCloakUserDeletionException extends RuntimeException {
+    public KeyCloakUserDeletionException(String message) {
+        super(message);
+    }
+}

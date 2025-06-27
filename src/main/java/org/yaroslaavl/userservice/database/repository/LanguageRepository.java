086c6b44296fe7b920f847182a8e4f4d61a834cf
Yaroslav Lopatkin <ylopatkin@gmail.com>
2025-06-27 18:44:19 +0200

    27.06.2025 - created repository layer for entities

---
+++ src/main/java/org/yaroslaavl/userservice/database/repository/LanguageRepository.java
@@ -0,0 +1,19 @@
+package org.yaroslaavl.userservice.database.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.yaroslaavl.userservice.database.entity.CandidateLanguage;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<CandidateLanguage, UUID> {
+
+    @Modifying
+    @Query("DELETE FROM CandidateLanguage cl WHERE cl.id NOT IN (:uuids)")
+    void deleteAllWhereIdsIsNotLike(@Param("uuids") Set<UUID> uuids);
+}

af862d281304ba30e7b379af60061441042c4e7e
ylopatkin <ylopatkin@gmail.com>
2025-09-13 21:43:18 +0200

    13.09 - updated mappers with new DTOs and mappings

--- src/main/java/org/yaroslaavl/userservice/mapper/CompanyMapper.java
+++ src/main/java/org/yaroslaavl/userservice/mapper/CompanyMapper.java
@@ -1,14 +1,41 @@
 package org.yaroslaavl.userservice.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.yaroslaavl.userservice.database.entity.Company;
 import org.yaroslaavl.userservice.dto.integrations.CompanyExecutedDto;
-import org.yaroslaavl.userservice.dto.read.CompanyReadDto;
+import org.yaroslaavl.userservice.dto.response.CompanyResponseDto;
+import org.yaroslaavl.userservice.dto.response.list.CompanyShortDto;
+import org.yaroslaavl.userservice.feignClient.dto.CompanyPreviewFeignDto;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface CompanyMapper {
 
     CompanyExecutedDto toExecutedDto(Company company);
 
-    CompanyReadDto toDto(Company company);
+    CompanyResponseDto toDto(Company company);
+
+    @Mapping(target = "vacancies", expression = "java(getCompanyVacanciesCount(company, companyVacanciesCount))")
+    CompanyShortDto toShortDto(Company company, Map<UUID, Long> companyVacanciesCount);
+
+    default List<CompanyShortDto> toShortDto(List<Company> companyList, Map<UUID, Long> companyVacanciesCount) {
+        return companyList.stream()
+                .map(company -> toShortDto(company, companyVacanciesCount))
+                .toList();
+    }
+
+    @Mapping(target = "location", expression = "java(location(company))")
+    CompanyPreviewFeignDto toPreviewFeignDto(Company company);
+
+    default String location(Company company) {
+        return company.getCity() + ", " +  company.getStreet();
+    }
+
+    default Long getCompanyVacanciesCount(Company company, Map<UUID, Long> companyVacanciesCount) {
+        return companyVacanciesCount.get(company.getId());
+    }
 }
--- src/main/java/org/yaroslaavl/userservice/mapper/RecruiterRegistrationRequestMapper.java
+++ src/main/java/org/yaroslaavl/userservice/mapper/RecruiterRegistrationRequestMapper.java
@@ -1,8 +1,30 @@
 package org.yaroslaavl.userservice.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.yaroslaavl.userservice.database.entity.RecruiterRegistrationRequest;
+import org.yaroslaavl.userservice.dto.response.RecruiterRegistrationRequestResponseDto;
+import org.yaroslaavl.userservice.dto.response.list.RecruiterRegistrationRequestShortDto;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RecruiterRegistrationRequestMapper {
 
+    @Mapping(target = "recruiterEmail", source = "recruiter.email")
+    @Mapping(target = "companyName", source = "company.name")
+    @Mapping(target = "reviewedBy", expression = "java(getReviewerFirstAndLastName(recruiterRegistrationRequest))")
+    RecruiterRegistrationRequestShortDto toShortDto(RecruiterRegistrationRequest recruiterRegistrationRequest);
+
+    List<RecruiterRegistrationRequestShortDto> toShortDto(List<RecruiterRegistrationRequest> recruiterRegistrationRequests);
+
+    @Mapping(target = "recruiterId", source = "recruiter.id")
+    @Mapping(target = "companyName", source = "company.name")
+    @Mapping(target = "companyStatus", source = "company.companyStatus")
+    @Mapping(target = "reviewedBy", expression = "java(getReviewerFirstAndLastName(recruiterRegistrationRequest))")
+    RecruiterRegistrationRequestResponseDto toDto(RecruiterRegistrationRequest recruiterRegistrationRequest);
+
+    default String getReviewerFirstAndLastName(RecruiterRegistrationRequest recruiterRegistrationRequest) {
+        return recruiterRegistrationRequest.getReviewedBy().getFirstName() + " " + recruiterRegistrationRequest.getReviewedBy().getLastName();
+    }
 }

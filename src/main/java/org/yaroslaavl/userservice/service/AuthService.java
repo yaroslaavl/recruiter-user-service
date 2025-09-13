385d93035fe3ca1e7dea45cbd28e85ee7c53702e
ylopatkin <ylopatkin@gmail.com>
2025-09-13 21:44:44 +0200

    13.09 - migrated service interfaces to updated response DTOs and added new methods for fetching filtered and detailed user, company, and recruiter data

--- src/main/java/org/yaroslaavl/userservice/service/AuthService.java
+++ src/main/java/org/yaroslaavl/userservice/service/AuthService.java
@@ -1,14 +1,14 @@
 package org.yaroslaavl.userservice.service;
 
 import org.yaroslaavl.userservice.dto.integrations.CompanyExecutedDto;
-import org.yaroslaavl.userservice.dto.read.CandidateReadDto;
-import org.yaroslaavl.userservice.dto.read.RecruiterReadDto;
+import org.yaroslaavl.userservice.dto.response.CandidateResponseDto;
+import org.yaroslaavl.userservice.dto.response.RecruiterResponseDto;
 import org.yaroslaavl.userservice.dto.registration.CandidateRegistrationDto;
 import org.yaroslaavl.userservice.dto.registration.RecruiterRegistrationDto;
 
 public interface AuthService {
 
-    CandidateReadDto createCandidateAccount(CandidateRegistrationDto candidateRegistrationDto);
+    CandidateResponseDto createCandidateAccount(CandidateRegistrationDto candidateRegistrationDto);
 
-    RecruiterReadDto createRecruiterAccount(RecruiterRegistrationDto recruiterRegistrationDto, CompanyExecutedDto companyExecutedDto);
+    RecruiterResponseDto createRecruiterAccount(RecruiterRegistrationDto recruiterRegistrationDto, CompanyExecutedDto companyExecutedDto);
 }

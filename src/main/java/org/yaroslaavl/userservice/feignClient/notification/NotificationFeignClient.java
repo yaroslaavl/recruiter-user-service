3df15ff77daac7c553e136890c716fbb091cfb81
ylopatkin <ylopatkin@gmail.com>
2025-09-13 21:43:34 +0200

    13.09 - added new DTOs and Feign clients for user, company, notification, and recruiting services

--- src/main/java/org/yaroslaavl/userservice/feignClient/notification/NotificationFeignClient.java
+++ src/main/java/org/yaroslaavl/userservice/feignClient/notification/NotificationFeignClient.java
@@ -1,11 +1,11 @@
-package org.yaroslaavl.userservice.feignClient.email;
+package org.yaroslaavl.userservice.feignClient.notification;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "notification-service", path = "/api/v1")
-public interface EmailFeignClient {
+public interface NotificationFeignClient {
 
     @GetMapping("/mail/check")
     String checkEmailVerification(@RequestParam("email") String email);
